0bda2d4cc0cc99e3a0a6a76df81396344382ec95
Yanni Etchi <81278130+YanniEtchi237@users.noreply.github.com>
2024-02-16 10:04:19 -0500

    final

--- predictor.py
+++ predictor.py
@@ -4,75 +4,114 @@ import sys
 import os
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.applications.resnet50 import ResNet50
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
+import cv2
+import traceback
+import pickle
+import warnings
 
-# Load the pre-trained ResNet50 model
-def load_resnet_model():
-    base_model = ResNet50(weights='imagenet')
-    model = Model(inputs=base_model.input, outputs=base_model.layers[-2].output)
-    return model
 
-# Load the pre-trained LSTM captioning model
-def load_lstm_model():
-    return load_model('models/lstm_model.h5')
+# Load the pre-trained ResNet50 model or pre-trained LSTM captioning model
 
-# Preprocess the input image
-def preprocess_image(image_path):
-    img = image.load_img(image_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
-    return img_array
+def load_model(model_path):
+    try:
+        model = tf.keras.models.load_model(model_path,compile= False)
+        return model
+    except Exception as e:
+        print(f"Error loading the model: {e}")
+        traceback.print_exc()
+        sys.exit(1)
+
 
 # Generate image features using the ResNet model
 def extract_image_features(model, image_path):
-    img_array = preprocess_image(image_path)
-    features = model.predict(img_array)
-    return features
+    image_features = {}
+    
+    #read image using cv2
+    img = cv2.imread(image_path)
+    #Change image color to RGB
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # preprocess Image to shape used by model
+    img = cv2.resize(img, (224,224)).reshape(1,224,224,3)
+    
+    #model prediction
+    pred = model.predict(img,verbose = 0).reshape(1,2048)
+
+
+    #grab only name of image without path   
+    img_name = image_path.split('/')[-1]
+
+    
+    #save pred array to dictionary
+    image_features[img_name] = pred
+
+    return image_features
+
+
 
 # Generate a caption for the image using the LSTM model
-def generate_caption(lstm_model, image_features):
-    word_to_index = {'startseq': 1, 'endseq': 2}  # Update with your vocabulary
-    index_to_word = {1: 'startseq', 2: 'endseq'}  # Update with your vocabulary
-    max_length = 50  # Maximum length of the generated caption
+def generate_caption(model, image_features):
 
-    caption = 'startseq'
 
-    for _ in range(max_length):
-        sequence = [word_to_index[word] for word in caption.split() if word in word_to_index]
-        sequence = pad_sequences([sequence], maxlen=max_length)
+    #import dicitonary 
+    with open('./dictionary/inv_dict.pkl', 'rb') as f:
+        inv_dict = pickle.load(f)
 
-        prediction = lstm_model.predict([np.array([image_features]), np.array(sequence)])
-        prediction = np.argmax(prediction)
-        
-        if prediction == 0:
-            continue
+    with open('./dictionary/word_dict.pkl', 'rb') as p:
+        word_dict = pickle.load(p)
 
-        word = index_to_word.get(prediction, '')
-        
-        if word == 'endseq':
+    text_inp = ['startofseq']
+
+    count = 0
+    caption = ''
+    while count < 25:
+        count += 1
+
+        encoded = []
+        for i in text_inp:
+            encoded.append(word_dict[i])
+
+        encoded = [encoded]
+
+        encoded = tf.keras.preprocessing.sequence.pad_sequences(encoded, padding='post', truncating='post', maxlen=36)
+
+
+        prediction = np.argmax(model.predict([image_features, encoded],verbose = 0))
+
+        sampled_word = inv_dict[prediction]
+
+        if sampled_word == 'endofseq':
             break
-        
-        caption += ' ' + word
-    
+
+        caption = caption + ' ' + sampled_word
+            
+       
+
+        text_inp.append(sampled_word)
+
     return caption
 
-# Main function to run the prediction
-def predict_caption(image_path):
-    resnet_model = load_resnet_model()
-    lstm_model = load_lstm_model()
 
-    image_features = extract_image_features(resnet_model, image_path)
-    caption = generate_caption(lstm_model, image_features)
 
     return caption
 
 if __name__ == "__main__":
+
+    warnings.filterwarnings("ignore", category=FutureWarning)
+
     image_path = sys.argv[1]
-    prediction = predict_caption(image_path)
-    print(prediction)
+    #load resnet
+
+    resnet = load_model("./models/resnet.h5")
+
+    lstm = load_model("./models/model.h5")
+
+    #extract image features
+
+    img_feat=extract_image_features(resnet,image_path)
+
+    #generate caption
+
+    caption = generate_caption(lstm,img_feat)
+    
+    
+    print(caption)
