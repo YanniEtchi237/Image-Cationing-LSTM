dd5a96f5322680f2147639c1fd94730cf69ded08
Yanni Etchi <81278130+YanniEtchi237@users.noreply.github.com>
2024-02-16 20:30:49 -0500

    final

--- public/script.js
+++ public/script.js
@@ -1,4 +1,55 @@
 // frontend/script.js
+document.getElementById('fileDropArea').addEventListener('click', () => {
+  document.getElementById('fileInput').click();
+});
+
+document.getElementById('fileInput').addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    
+    document.querySelector('.file-drop-text').textContent = file.name; // Update text to show the file name
+  }
+});
+
+// Drag and drop events
+const fileDropArea = document.getElementById('fileDropArea');
+
+fileDropArea.addEventListener('dragenter', (event) => {
+  event.preventDefault();
+  fileDropArea.classList.add('dragover');
+});
+
+fileDropArea.addEventListener('dragover', (event) => {
+  event.preventDefault();
+  fileDropArea.classList.add('dragover');
+});
+
+fileDropArea.addEventListener('dragleave', () => {
+  fileDropArea.classList.remove('dragover');
+});
+
+fileDropArea.addEventListener('drop', (event) => {
+  event.preventDefault();
+  fileDropArea.classList.remove('dragover');
+
+  const file = event.dataTransfer.files[0];
+  if (file) {
+    document.querySelector('.file-drop-text').textContent = file.name; // Update text to show the file name
+  }
+});
+
+document.getElementById('uploadButton').addEventListener('click', () => {
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+  if (file) {
+    uploadFile(file);
+  } else {
+    alert('Please select an image file.');
+  }
+});
+
+
+
 document.getElementById('uploadButton').addEventListener('click', () => {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
@@ -22,6 +73,8 @@ document.getElementById('uploadButton').addEventListener('click', () => {
       const predictionResult = document.getElementById('predictionResult');
       predictionResult.textContent = 'Prediction: ' + data.prediction;
     })
+
+    
     .catch(error => {
       console.error('Error uploading file: ', error);
       alert('Error uploading file. Please try again.');
